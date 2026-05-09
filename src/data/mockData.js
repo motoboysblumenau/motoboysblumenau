c5c2861773338e443364757fb8f53ae4694b6982
motoboysblumenau <motoboysblumenau@gmail.com>
2026-05-09 16:08:59 -0300

    Publica site MB EXPRESS

--- src/data/mockData.js
+++
@@ -1,5 +0,0 @@
-import deliveryRequests from './deliveryRequests.json';
-import motoboys from './motoboys.json';
-
-export const mockMotoboys = motoboys;
-export const mockDeliveryRequests = deliveryRequests;
